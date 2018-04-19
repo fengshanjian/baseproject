b5c21c87ae162f5f69069b391cf3dd1c8cb4bf10
fengshanjian <wentaozhao@163.com>
2018-04-19 14:55:19 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ else
     npm install --save teaset
     npm install react-native-animatable --save
     npm install react-native-vector-icons --save 
-    npm i --save react-native-elements
+    npm i --save react-native-elements@1.0.0-beta4
     npm install babel-plugin-transform-decorators-legacy --save-dev
     # 使 eslint 支持 AirBnb 编码规范和ES7 编码规范,
     export PKG=eslint-config-airbnb;
